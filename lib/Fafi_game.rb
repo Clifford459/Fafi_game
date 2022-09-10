3dc50058fe5411170d148897dbdbfc61760d791c
Clifford459 <clifford459@outlook.com>
2022-09-10 20:17:59 +0200

    Added restrictions to input

--- lib/Fafi_game.rb
+++ lib/Fafi_game.rb
@@ -1,25 +1,56 @@
 class Game
   def initialize(try1, try2)
-    @try1  = try1
-    @try2  = try2
-    @code1 = (1..30).to_a.sample(3).join(", ")
-    @code2 = (1..10).to_a.sample(3).join(", ")
+    @try1  = try1.to_i
+    @try2  = try2.to_i
+    @code1 = (1..30).to_a.sample(3)
+    @code2 = (1..10).to_a.sample(3)
   end
 
-  def win?
-    if @try1 == @code1 || @try2 == @code2
+  #def valid_input1?
+  #  if @try1 > 30 || @try1 == 0
+  #    return false
+  #  end
+  #  true
+  #end
+
+  def valid_input1?
+    if @try1 <= 30 && @try1 != 0
       return true
     end
-    return false
+    false
   end
 
+  def valid_input2?
+    if @try2 <= 10 && @try2 != 0
+      return true
+    end
+    false
+  end
+
+  def win_round_1?
+    @code1.map do |n|
+      if @try1 == n
+        return true
+      end
+    end
+    false
+  end
+
+  def win_round_2?
+    @code2.map do |n|
+      if @try2 == n
+        return true
+      end
+    end
+    false
+  end
 
   def code1
-    return @code1
+    return @code1.join(", ")
   end
 
   def code2
-    return @code2
+    return @code2.join(", ")
   end
 
   def try1
--- main.rb
+++ main.rb
@@ -1,17 +1,28 @@
 require "./lib/Fafi_game.rb"
 
 puts "Choose any number between 1 and 30 for your first round"
-input_try1   = gets.chomp()
+input_try1 = gets.chomp().to_i
 
 puts "Choose any number between 1 and 10 for your second round"
-input_try2   = gets.chomp()
+input_try2 = gets.chomp().to_i
 
 game = Game.new(input_try1, input_try2)
 
-if game.win?
-  puts 'You won'
+if game.valid_input1? != true
+  puts "Not a valid input for round 1"
+end
+
+if game.valid_input2? != true
+  puts "Not a valid input for round 2"
+end
+
+if game.win_round_1?
+  puts "You won in round 1"
+  puts "(#{input_try1}, #{input_try2}) and (#{game.code1}), (#{game.code2})"
+elsif game.win_round_2?
+  puts "You won in round 2"
   puts "(#{input_try1}, #{input_try2}) and (#{game.code1}), (#{game.code2})"
 else
   puts "You did not win anything"
-  puts "(#{input_try1}, #{input_try2}) and (#{game.code1}), (#{game.code2})"
+  puts "Your input(#{input_try1}, #{input_try2}) output (#{game.code1}), (#{game.code2})"
 end
